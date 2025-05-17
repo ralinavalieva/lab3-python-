3263eade8159066425e25bb9e721093ee517acaf
ralinavalieva <r04161767@gmail.com>
2025-05-17 16:34:57 +0300

    Update task1.2.py

--- task1.2.py
+++ task1.2.py
@@ -2,56 +2,32 @@
 
 class Student:
     def __init__(self, name, student_id):
-        """
-        инициализация студента.
-        
-        :param name: Имя студента
-        :param student_id: ID студента
-        """
-        self.name = name
-        self.student_id = student_id
-        self.grades = []
+        self.name = name          # сохраняем name в текущем объекте
+        self.student_id = student_id  # сохраняем student_id в текущем объекте
+        self.grades = []          # создаем пустой список grades для текущего объекта
 
-    def add_grade(self, grade):
-        """
-        добавляет оценку с валидацией (0-10).
-        
-        :param grade: оценка для добавления
-        """
-        if 0 <= grade <= 10:
-            self.grades.append(grade)
+    def add_grade(self, grade): # Добавляем оценку в список оценок студента
+        if 0<=grade<=10: # Проверяем, что оценка находится в допустимом диапазоне (0-10)
+            self.grades.append(grade) # Добавляем длпустимую оценку
         else:
-            raise ValueError("оценка должна быть в диапазоне от 0 до 10.")
-
-    def get_average(self):
-        """
-        возвращает средний балл.
-        
-        :return: средний балл
-        """
-        return sum(self.grades) / len(self.grades) if self.grades else 0
-
+            print('Ошибка. Оценка не в диапазоне 0-10') # Если оценка вне диапазона, выводит сообщение об ошибке
+            
+    def get_average(self): # Вычисляем средний балл студента
+        if not self.grades:
+            return 0.0 # Если нет оценок, возвращаем 0.0
+        return sum(self.grades)/len(self.grades) # Иначе возвращаем среднее арифметическое всех оценок
+    
     def display_info(self):
-        """
-        выводит информацию о студенте.
-        """
-        print(f"имя: {self.name}, ID: {self.student_id}, оценки: {self.grades}, средний балл: {self.get_average():.2f}")
-
-    def __str__(self):
-        return f"студент {self.name} (ID: {self.student_id})"
-
-    def __eq__(self, other):
-        return self.student_id == other.student_id
-
-    def __len__(self):
-        return len(self.grades)
-
-
-if __name__ == "__main__":
-    student = Student("Иван Иванов", "12345")
-    student.add_grade(8)
-    student.add_grade(9)
-    student.display_info()
+        print(f'Студент: {self.name}')
+        print(f'ID: {self.student_id}')
+        print(f"Оценки: {', '.join(map(str,self.grades))}") # Список оценок (преобразованный в строку)
+        print(f'Средний балл: {self.get_average():.2f}\n') # Средний балл (с округлением до 2 знаков после запятой)
+        
+if __name__=="__main__":
+    student1 = Student("Краснова Полина", "202")
+    student1.add_grade(3)
+    student1.add_grade(8)
+    student1.display_info()
     
     
 # task2.py
