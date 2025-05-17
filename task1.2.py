38aacdfa53baeb877e8f27547fbb0bc323247d8f
ralinavalieva <r04161767@gmail.com>
2025-05-17 17:13:08 +0300

    Update task1.2.py

--- task1.2.py
+++ task1.2.py
@@ -1,23 +1,23 @@
 # task1.py
 
 class Student:
-    def __init__(self, name, student_id):
+    def __init__(self, name, student_id):#метод, кот вызыв-ся авт-ки при создаии нового обьекта
         self.name = name          # сохраняем name в текущем объекте
         self.student_id = student_id  # сохраняем student_id в текущем объекте
         self.grades = []          # создаем пустой список grades для текущего объекта
 
-    def add_grade(self, grade): # Добавляем оценку в список оценок студента
+    def add_grade(self, grade): # Добавляем оценку в список оценок студента, собирает все оценки и вычисл их ср зн
         if 0<=grade<=10: # Проверяем, что оценка находится в допустимом диапазоне (0-10)
             self.grades.append(grade) # Добавляем длпустимую оценку
         else:
             print('Ошибка. Оценка не в диапазоне 0-10') # Если оценка вне диапазона, выводит сообщение об ошибке
             
-    def get_average(self): # Вычисляем средний балл студента
+    def get_average(self): # Вычисляем средний балл студента, возвр ср ар всех оценок в списке self.grades
         if not self.grades:
             return 0.0 # Если нет оценок, возвращаем 0.0
         return sum(self.grades)/len(self.grades) # Иначе возвращаем среднее арифметическое всех оценок
     
-    def display_info(self):
+    def display_info(self):#выводит и возвр на экран инфо о сост обьекта
         print(f'Студент: {self.name}')
         print(f'ID: {self.student_id}')
         print(f"Оценки: {', '.join(map(str,self.grades))}") # Список оценок (преобразованный в строку)
@@ -44,15 +44,9 @@ class Person:
         self.age = age
 
 
-class Teacher(Person):
+class Teacher(Person):#наслед чел
     def __init__(self, name, age, subject):
-        """
-        инициализация преподавателя.
-        
-        :param name: имя преподавателя
-        :param age: возраст преподавателя
-        :param subject: преподаваемый предмет
-        """
+    
         super().__init__(name, age)
         self.subject = subject
         self.students = []
